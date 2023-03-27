5d253bc8f81267a0e489845b6f1264cb0a2b5f49
Malgorzatta <126244388+Malgorzatta@users.noreply.github.com>
2023-03-27 22:34:59 +0200

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,13 @@
+from turtle import *
+pensize(3)
+color('gold', 'powder blue')
+begin_fill()
+left(40)
+forward(60)
+circle(30, 200)
+left(240)
+circle(30, 200)
+forward(60)
+end_fill()
+hideturtle()
+done()
